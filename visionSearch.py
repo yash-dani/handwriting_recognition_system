c6d0ff47515fe4632d59a03c54d7d0ba82d45561
Yash Dani <41090933+yash-dani@users.noreply.github.com>
2020-01-07 18:06:37 -0500

    added more in-depth comments

--- visionSearch.py
+++ visionSearch.py
@@ -1,20 +1,26 @@
+# Import statements
 from google.cloud import vision
 import os
 import io
+
+# Imports the JSON key required to call the API, which can be generated on the Google Cloud Console
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="client-secret.json"
 
-
+# Function to detect text using a local path
 def detect_text_uri(path):
+	# Creates the API client
 	client = vision.ImageAnnotatorClient()
 	
 	with io.open(path, 'rb') as image_file:
 		content = image_file.read()
+		
+	# Detects text and stores in JSON object 
 	image = vision.types.Image(content=content)
 	response = client.text_detection(image=image)
 	texts = response.text_annotations
+	
+	# returns raw text data 
 	return texts[0].description
 	
-
+# Quick example of usecase 
 #detect_text_uri("./IMG-0092.jpg")
-#detect_text_uri("https://media.npr.org/assets/img/2016/04/17/handwritten-note_wide-941ca37f3638dca912c8b9efda05ee9fefbf3147.jpg?s=1400")
-print("text message twilio")
